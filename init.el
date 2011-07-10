a9a3a990dcd0b1b4af13a745009b96e4b940461b
Thomas Kappler <tkappler@googlemail.com>
2011-07-10 12:49:29 +0200

    Cleanup.

--- init.el
+++ init.el
@@ -472,13 +472,11 @@ line."
  '(TeX-PDF-mode t)
  '(delete-selection-mode nil)
  '(mark-even-if-inactive t)
- '(scroll-bar-mode (quote right))
  '(transient-mark-mode 1))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:height 101 :family "Inconsolata"))))
  '(writegood-passive-voice-face ((((class color)) (:background "moccasin"))))
  '(writegood-weasels-face ((((class color) (background light)) (:background "moccasin")))))
